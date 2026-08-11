e8602c254feb5aab0664494ef8cfb8f2b11c4c77
Chitraksic18 <chitrakshi.25bai11264@vitbhoal.ac.in>
2026-08-11 14:42:29 +0530

    add java exercises

---
+++ class/FinalDemo.java
@@ -0,0 +1,6 @@
+public class FinalDemo {
+    public static void main(String[]args) {
+        final double PI =3.14159;
+        System.out.println("value of PI="+PI);//System.out.println("value of PI="+9PI+1);
+    }
+}
